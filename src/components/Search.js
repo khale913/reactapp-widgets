6bee622fd975fbab39566ea144775a4bea351cc5
Kelton Hale <keltonhale33@gmail.com>
2021-11-15 11:20:30 -0700

    rendered each result with search

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,8 +3,47 @@ import axios from "axios";
 
 const Search = (props) => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
 
-  useEffect(() => {}, [term]);
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    if (term) {
+      search();
+    }
+  }, [term]);
+  console.log(results);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -18,6 +57,7 @@ const Search = (props) => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
